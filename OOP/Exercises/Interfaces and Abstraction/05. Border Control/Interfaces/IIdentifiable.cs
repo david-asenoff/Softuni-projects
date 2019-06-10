cbcd9ce831100db82a1a785dfb7d32b765bc4647
david-asenoff <david.asenoff@gmail.com>
2019-06-10 14:33:03 +0300

    Interfaces and Abstraction

---
+++ OOP/Exercises/Interfaces and Abstraction/05. Border Control/Interfaces/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05BorderControl.Interfaces
+{
+    public interface IIdentifiable
+    {
+        string Id { get; }
+    }
+}
