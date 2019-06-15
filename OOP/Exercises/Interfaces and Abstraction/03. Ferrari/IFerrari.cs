afc48c09cc84b1bf5e91b2389f5a772628050091
david-asenoff <david.asenoff@gmail.com>
2019-06-15 14:32:05 +0300

    Interfaces and Abstraction

---
+++ OOP/Exercises/Interfaces and Abstraction/03. Ferrari/IFerrari.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.Ferrari
+{
+    public interface IFerrari
+    {
+        string Model {get; }
+        string DriversName { get; }
+    }
+}
