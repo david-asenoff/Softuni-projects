c0ba88b09779141253373bd870bdcaa12e8f2856
david-asenoff <david.asenoff@gmail.com>
2019-06-17 14:30:52 +0300

    Interfaces and Abstraction

---
+++ OOP/Exercises/Interfaces and Abstraction/01. Define an Interface IPerson/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IPerson
+    {
+        string Name { get; }
+        int Age { get; }
+    }
+}
