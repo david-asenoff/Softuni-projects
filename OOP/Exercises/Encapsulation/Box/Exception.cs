f0b402609aabf0e919f2f027eb821b8928706348
david-asenoff <david.asenoff@gmail.com>
2019-06-22 10:12:47 +0300

    Box

---
+++ OOP/Exercises/Encapsulation/Box/Exception.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public static class Exception
+    {
+        public static string CanNotBeZeroOrNegative = " cannot be zero or negative.";
+    }
+}
