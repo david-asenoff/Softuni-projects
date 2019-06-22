0fcf375295953e5458cb9b2fa98c8add143a1f20
david-asenoff <david.asenoff@gmail.com>
2019-06-22 14:18:48 +0300

    Polymorphism

---
+++ OOP/Exercises/Polymorphism/01. Vehicles/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Vehicles.Exceptions
+{
+    static class ExceptionMessages
+    {
+        //public static string NotEnoughFuel = "{0} needs refueling";
+    }
+}
