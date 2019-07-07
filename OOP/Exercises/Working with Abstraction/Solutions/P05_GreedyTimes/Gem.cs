a5da0f8f54a82f17bbe5ca06077f40c490be7a49
david-asenoff <david.asenoff@gmail.com>
2019-07-07 20:23:11 +0300

    Greedy Times

---
+++ OOP/Exercises/Working with Abstraction/Solutions/P05_GreedyTimes/Gem.cs
@@ -0,0 +1,15 @@
+﻿namespace GreedyTimes
+{
+    public class Gem
+    {
+        public Gem(string name, long quantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; }
+
+        public long Quantity { get; set; }
+    }
+}
