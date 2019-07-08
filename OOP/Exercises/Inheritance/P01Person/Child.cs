5f880147d9ff8704f38e169026896e3043bd77a6
david-asenoff <david.asenoff@gmail.com>
2019-07-08 12:54:44 +0300

    Person

---
+++ OOP/Exercises/Inheritance/P01Person/Child.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException(Exception.ChildCannotBeAboveFifteen());
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
