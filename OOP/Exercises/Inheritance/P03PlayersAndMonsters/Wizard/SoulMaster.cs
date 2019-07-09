f55faa6b599439d12f8d45d49e8ca9f31398bfda
david-asenoff <david.asenoff@gmail.com>
2019-07-09 13:51:54 +0300

    Heroes

---
+++ OOP/Exercises/Inheritance/P03PlayersAndMonsters/Wizard/SoulMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Wizard
+{
+    class SoulMaster:Wizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+        }
+    }
+}
