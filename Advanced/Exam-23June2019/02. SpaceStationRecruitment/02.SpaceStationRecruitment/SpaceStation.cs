f2d4f56cfe889f3a0e57756496ff884a1add745c
david-asenoff <david.asenoff@gmail.com>
2019-08-02 12:16:07 +0300

    Exam problem

---
+++ Advanced/Exam-23June2019/02. SpaceStationRecruitment/02.SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        List<Astronaut> data;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public SpaceStation(string name, int capacity)
+        {
+            data = new List<Astronaut>();
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+
+        public void Add(Astronaut astronaut)
+        {
+            if (data.Count<Capacity)
+            {
+                data.Add(astronaut);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            foreach (var astronaut in data)
+            {
+                if (astronaut.Name==name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut oldest = data.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldest;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            Astronaut ourGuy = data.Where(x=>x.Name==name).FirstOrDefault();
+            return ourGuy;
+        }
+
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (var astronaut in data)
+            {
+                sb.AppendLine($"{astronaut}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
