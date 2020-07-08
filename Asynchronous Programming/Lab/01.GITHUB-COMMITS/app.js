cfed784da10a62a713bf11c8607adf8175a5f9ed
David Asenov <david.asenoff@gmail.com>
2020-07-08 18:25:12 +0300

    Solve Github commits

--- Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
+++ Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,47 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+
+    const username = document.getElementById('username');
+    const repo = document.getElementById('repo');
+    const commitsUl = document.getElementById('commits');
+    const url = `https://api.github.com/repos/${username.value}/${repo.value}/commits`;
+
+    commitsUl.innerHTML = '';
+
+    try {
+        const response = await fetch(url);
+
+        if (response.status == 200) {
+
+            const reposJson = await response.json();
+
+            Object.entries(reposJson)
+                .forEach(([index, currentRepo]) => {
+
+                    const li = document.createElement('li');
+                    li.textContent = `${currentRepo.commit.author.name}: ${currentRepo.commit.message}`;
+                    commitsUl.appendChild(li);
+                });
+
+            username.value = '';
+            repo.value = '';
+
+        } else if (!response.ok) {
+
+            throw new Error(JSON.stringify({
+
+                status: response.status,
+                statusText: response.statusText
+            }));
+        }
+
+    } catch (data) {
+
+        const error = JSON.parse(data.message);
+        const li = document.createElement('li');
+        li.textContent = `Error: ${error.status} (${error.statusText})`;
+        commitsUl.appendChild(li);
+
+        username.value = '';
+        repo.value = '';
+    }
 }
