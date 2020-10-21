11dc81101487fecfa06cc3b1a7a56a50ce0b8e91
David Asenov <david.asenoff@gmail.com>
2020-10-21 13:40:25 +0300

    Add Db entities and Initial migration

--- Exam Preparation - 20.10.2020/Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Exam Preparation - 20.10.2020/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -12,5 +12,20 @@
             : base(db)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=SharedTrip;Integrated Security=True");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(x => new { x.TripId, x.UserId });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Exam Preparation - 20.10.2020/Apps/SharedTrip/Data/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SharedTrip.Data
+{
+    public class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+            UserTrips = new HashSet<UserTrip>();
+        }
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(5)]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(6)]
+        public string Password { get; set; }
+        public virtual ICollection<UserTrip> UserTrips { get; set; }
+    }
+}
