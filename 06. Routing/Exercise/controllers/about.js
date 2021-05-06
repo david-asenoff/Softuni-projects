cd4ab57a5ce241c472ddf8c9331d0d53839a8e6a
David Asenov <49104423+david-asenoff@users.noreply.github.com>
2021-05-06 23:44:20 +0300

    Delete 06. Routing/Exercise directory

--- 06. Routing/Exercise/controllers/about.js
+++
@@ -1,9 +0,0 @@
-export default async function() {
-                                             // this === Sammy.RenderContext, рендериране на темплейти
-    this.partials = {
-        header: await this.load('./templates/common/header.hbs'),
-        footer: await this.load('./templates/common/footer.hbs')
-    };
-
-    this.partial('./templates/about/about.hbs', this.app.userData);
-}
