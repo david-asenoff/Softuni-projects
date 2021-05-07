920c7a1cedc42bbc290ded4d22aabd471ca230fc
David Asenov <49104423+david-asenoff@users.noreply.github.com>
2021-05-07 19:44:01 +0300

    Delete 08. Reactive-Jokes directory

--- 08. Reactive-Jokes/05. Components - Deep dive/src/services/bookService.js
+++
@@ -1,9 +0,0 @@
-function getAll() {
-    return fetch('http://localhost:3000/books')
-        .then(res => res.json())
-        .catch(error => console.log(error));
-}
-
-export default {
-    getAll
-}
